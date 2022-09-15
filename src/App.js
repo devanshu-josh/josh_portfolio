d88d95d272fad67ed28c5092a2a68fb478951f67
aditya-ragi <6265220110a@gmail.com>
2022-09-15 13:57:29 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar';
+
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-slate-700 ">
-      Hello world!
-    </h1>
-       <h1 className="text-slate-700 ">
-      chodu bhaiya
-    </h1>
+    <div>
+       <Navbar/>
     </div>
   );
 }
