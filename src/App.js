23c699bd9d7e706af14787c6e03876e8eab8bd9d
Devanshu Ragi <devanshuragi456@gmail.com>
2022-09-15 19:15:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import Navbar from './components/navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-       <h1 className="text-slate-700 ">
-      Hello world!
-    </h1>
-       <h1 className="text-slate-700 ">
-      Josh IT works
-    </h1>
-=======
     <div>
        <Navbar/>
->>>>>>> master
     </div>
   );
 }
